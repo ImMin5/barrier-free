a5f9e392a3ced4e31452f2056385764f25803c8a
ImMino <rlaalsgh5151@naver.com>
2022-04-07 22:59:31 +0900

    [FEAT] 숙박정보 Controller 파라미터 포함여부 및 기본값 설정변경

--- src/main/java/com/team/bf/controller/AccommodationController.java
+++ src/main/java/com/team/bf/controller/AccommodationController.java
@@ -15,10 +15,13 @@ public class AccommodationController {
     OpenApiService openApiService;
 
     @GetMapping("/accommodation")
-    public ModelAndView main(@RequestParam(value = "pageNo")String pageNo, @RequestParam(value = "pageCount")String pageCount, @RequestParam(value = "searchWord")String searchWord){
+    public ModelAndView main(@RequestParam(value = "pageNo", required = false, defaultValue = "1")String pageNo, @RequestParam(value = "pageCount",required = false,  defaultValue = "10")String pageCount, @RequestParam(value = "searchWord",required = false,  defaultValue = "")String searchWord){
         ModelAndView mav = new ModelAndView();
         System.out.println("pagNO "+ pageNo);
-        
+
+        if(pageNo == null)pageNo = "1";
+        if(pageCount == null)pageCount = "10";
+        if(searchWord == null)searchWord="";
         mav.addObject("accomoList", openApiService.TourTypeInfo(pageNo, pageCount, "32",searchWord));
         mav.setViewName("accommodation/accommodationPage");
         return mav;
