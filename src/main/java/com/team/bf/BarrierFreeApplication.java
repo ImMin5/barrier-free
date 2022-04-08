10198b5094dd37ca3ed9ecdb41ae0245a601d8fb
ImMino <rlaalsgh5151@naver.com>
2022-04-08 16:40:22 +0900

    [ENV] ide sts 로 변경

--- src/main/java/com/team/bf/BarrierFreeApplication.java
+++ src/main/java/com/team/bf/BarrierFreeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BfApplication {
+public class BarrierFreeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BfApplication.class, args);
+		SpringApplication.run(BarrierFreeApplication.class, args);
 	}
 
 }
--- src/main/webapp/WEB-INF/views/accommodation/accommodationPage.jsp
+++ src/main/webapp/WEB-INF/views/accommodation/accommodationPage.jsp
@@ -23,4 +23,14 @@
 	
 		</form>
 	</div>
+	<div>
+		<ul>
+			<c:forEach var="vo" items="${accomoList}">
+				<li>${vo.get("title")}</li>
+				<li><c:if test="${vo.get('tel')}"></c:if></li>
+				<li>${vo.get("addr1")}</li>
+			</c:forEach>
+			
+		</ul>
+	</div>
 </div>
