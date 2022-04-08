295672264038d5cfa01bfaed19cbd5d7361daf02
ImMino <rlaalsgh5151@naver.com>
2022-04-08 17:01:17 +0900

    Init

--- src/main/java/com/team/bf/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package com.team.bf.service;
-
-import com.team.bf.vo.MemberVO;
-
-public interface MemberService {
-    public int memberInsert(MemberVO vo);
-    public MemberVO memberSelectById(String userid);
-    public int memberDelete(String userid, String password);
-}
