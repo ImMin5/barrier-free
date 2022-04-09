1b182f1776816fc2ff86a9f871c0d2e5e3f1b3f3
ImMino <rlaalsgh5151@naver.com>
2022-04-09 19:29:15 +0900

    [ENV] add gradle and set top,bottom jsp file

--- src/main/java/com/team/bf/ServletInitializer.java
+++ src/main/java/com/team/bf/ServletInitializer.java
@@ -1,13 +1,41 @@
 package com.team.bf;
 
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import java.util.Collections;
+
+
+import org.apache.catalina.Context;
+import org.apache.tomcat.util.descriptor.web.JspConfigDescriptorImpl;
+import org.apache.tomcat.util.descriptor.web.JspPropertyGroup;
+import org.apache.tomcat.util.descriptor.web.JspPropertyGroupDescriptorImpl;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+//@Slf4j
 public class ServletInitializer extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(BarrierFreeApplication.class);
-	}
-
+    @Bean
+    public ConfigurableServletWebServerFactory configurableServletWebServerFactory ( ) {
+        return new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                super.postProcessContext(context);
+                JspPropertyGroup jspPropertyGroup = new JspPropertyGroup();
+                jspPropertyGroup.addUrlPattern("*.jsp");
+                jspPropertyGroup.addUrlPattern("*.jspf");
+                jspPropertyGroup.setPageEncoding("UTF-8");
+                jspPropertyGroup.setScriptingInvalid("true");
+                jspPropertyGroup.addIncludePrelude("/WEB-INF/inc/top.jsp");
+                jspPropertyGroup.addIncludeCoda("/WEB-INF/inc/bottom.jsp");
+                jspPropertyGroup.setTrimWhitespace("true");
+                jspPropertyGroup.setDefaultContentType("text/html");
+                JspPropertyGroupDescriptorImpl jspPropertyGroupDescriptor = new JspPropertyGroupDescriptorImpl(jspPropertyGroup);
+                context.setJspConfigDescriptor(new JspConfigDescriptorImpl(Collections.singletonList(jspPropertyGroupDescriptor), Collections.emptyList()));
+            }
+        };
+    }
 }
