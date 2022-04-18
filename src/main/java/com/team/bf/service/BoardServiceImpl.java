0ad19679363649f5592637c7734ae75e536543ba
ImMino <rlaalsgh5151@naver.com>
2022-04-18 17:19:05 +0900

    [FEAT] mypage 문의사항 뷰 Controller 재연결 및 페이지네이션 기능 추가

--- src/main/java/com/team/bf/service/BoardServiceImpl.java
+++ src/main/java/com/team/bf/service/BoardServiceImpl.java
@@ -62,4 +62,9 @@ public class BoardServiceImpl implements BoardService {
 		return dao.boardAndReplySelectAll(vo);
 	}
 
+	@Override
+	public ArrayList<BoardAndReply> boardAndReplySelectById(PagingVO vo, String userid) {
+		return dao.boardAndReplySelectById(vo, userid);
+	}
+
 }
